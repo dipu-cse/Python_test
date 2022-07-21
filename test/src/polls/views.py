77e55cb6d6b47286326d051b1c2ace7991609fe2
dipu-cse <droy112@gmail.com>
2022-07-21 16:26:51 +0600

    test folder deleted

--- test/src/polls/views.py
+++
@@ -1,9 +0,0 @@
-from django.shortcuts import render
-
-# my import
-from django.http import HttpResponse
-
-# Create your views here.
-
-def index(request):
-    return HttpResponse("Hello, world. You're in the polls index.")
